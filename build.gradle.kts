49a9576a5fe79c703c11fedcdac71d4bc1268174
pengfei.li <l_pengf@163.com>
2025-09-26 23:06:30 +0800

    support IDEA 2025.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.pengfei.li"
-version = "3.1.1"
+version = "3.2.0"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("252.*")
+        untilBuild.set("253.*")
     }
 
     signPlugin {
