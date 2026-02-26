91804b4baaeb3ff915096a68ba6096ffabd06c4d
pengfei.li <l_pengf@163.com>
2026-02-26 22:28:06 +0800

    support IDEA 2026.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.pengfei.li"
-version = "3.2.0"
+version = "2026.1.0"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("253.*")
+        untilBuild.set("261.*")
     }
 
     signPlugin {
